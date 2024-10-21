230ca157c3668f7e1b49a0fc91c9052b34ceaedf
Mahmoud Osama <mahmoudosama9441@gmail.com>
2024-10-22 02:54:21 +0300

    add user order

---
+++ Ecommerce-BookStore/Areas/Customer/Controllers/UserOrderController.cs
@@ -0,0 +1,42 @@
+﻿using Ecommerce.DataAccess.Repositories;
+using Ecommerce.Entities.Interfaces;
+using Ecommerce.Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Ecommerce.Areas.User.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class UserOrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserOrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult UserOrders()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetUserOrders()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userOrders = _unitOfWork.OrderHeader.GetAll(
+                o => o.ApplicationUserId == userId,
+                Include: "ApplicationUser"
+            ).Select(order => new
+            {
+                id = order.Id,
+                orderDate = order.OrderDate,
+                orderStatus = order.OrderStatus,
+                totalPrice = order.TotalPrice
+            });
+
+            return Json(new { data = userOrders });
+        }
+    }
+}
