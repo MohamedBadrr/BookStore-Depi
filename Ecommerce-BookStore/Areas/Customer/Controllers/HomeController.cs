4ffc4d0ec2306644f20f908e9c985a80a6ecf052
MohamedBadrr <badr59497@gmail.com>
2024-10-21 05:42:27 +0300

    Add project files.

---
+++ Ecommerce-BookStore/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Ecommerce.Entities.Interfaces;
+using Ecommerce.Entities.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Uitilites;
+using X.PagedList.Extensions;
+
+namespace Ecommerce.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index(int? page)
+        {
+            var PageNumber = page ?? 1;
+            int PageSize = 8; 
+            var products = _unitOfWork.Product.GetAll().ToPagedList(PageNumber,PageSize);
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Details([FromRoute]int id)
+        {
+            ShoppingCart obj = new ShoppingCart()
+            {
+                ProductId = id,
+                Product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id, Include: "Category"),
+                Count = 1
+            };
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+           var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            shoppingCart.ApplicationUserId = claim.Value;
+            shoppingCart.Id = 0;
+
+            ShoppingCart cartobj = _unitOfWork.ShoppingCart.GetFirstOrDefault
+                (c => c.ApplicationUserId == shoppingCart.ApplicationUserId && c.ProductId == shoppingCart.ProductId);
+            if (cartobj == null)
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Complete();
+                HttpContext.Session.SetInt32(SD.SessionKey,
+                    _unitOfWork.ShoppingCart.GetAll(sc=>sc.ApplicationUserId == claim.Value).ToList().Count());
+               
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.IncreaseCount(cartobj, shoppingCart.Count);
+                _unitOfWork.Complete();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Ecommerce.DataAccess/Repositories/ProductRepository .cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.Entities.Models;
+using Ecommerce.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repositories
+{
+    public class ProductRepository : GenericRepository<Product> , IProductRepository
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public ProductRepository(ApplicationDbContext dbcontext) : base(dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public void Update(Product product)
+        {
+            var updatedProcuct = _dbcontext.Products.Find(product.Id);
+            if (updatedProcuct != null)
+            {
+                updatedProcuct.Name = product.Name;
+                updatedProcuct.Description = product.Description;
+                updatedProcuct.Price = product.Price;
+                updatedProcuct.Img = product.Img;
+                updatedProcuct.CategoryId = product.CategoryId;
+            }
+        }
+    }
+}
